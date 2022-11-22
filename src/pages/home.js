272d541b4c3a5debd3456090c915be7e17c0bcb9
sarahwxie <sarahwxie@gmail.com>
2022-11-22 15:57:56 -0800

    retyping affect with javascript

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,8 +1,67 @@
-import React from "react";
+import {React, useEffect} from "react";
 import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 
 // create a parallax homepage
 function HomePage() {
+    useEffect(() => {
+        // List of sentences
+        var _CONTENT = [ "Web Development", "Machine Learning", "Cybersecurity", "Blockchain/Web3" ];
+
+        // Current sentence being processed
+        var _PART = 0;
+
+        // Character number of the current sentence being processed
+        var _PART_INDEX = 0;
+
+        // Holds the handle returned from setInterval
+        var _INTERVAL_VAL;
+
+        // Element that holds the text
+        const element = document.querySelector("#typeText");
+
+        // Implements typing effect
+        function Type() {
+        	var text =  _CONTENT[_PART].substring(0, _PART_INDEX + 1);
+        	element.innerHTML = text;
+        	_PART_INDEX++;
+
+        	// If full sentence has been displayed then start to delete the sentence after some time
+        	if(text === _CONTENT[_PART]) {
+        		clearInterval(_INTERVAL_VAL);
+        		setTimeout(function() {
+        			_INTERVAL_VAL = setInterval(Delete, 50);
+        		}, 1000);
+        	}
+        }
+
+        // Implements deleting effect
+        function Delete() {
+        	var text =  _CONTENT[_PART].substring(0, _PART_INDEX - 1);
+        	element.innerHTML = text;
+        	_PART_INDEX--;
+
+        	// If sentence has been deleted then start to display the next sentence
+        	if(text === '') {
+        		clearInterval(_INTERVAL_VAL);
+
+        		// If last sentence then display the first one, else move to the next
+        		if(_PART === (_CONTENT.length - 1))
+        			_PART = 0;
+        		else
+        			_PART++;
+        		_PART_INDEX = 0;
+
+        		// Start to display the next sentence after some time
+        		setTimeout(function() {
+        			_INTERVAL_VAL = setInterval(Type, 100);
+        		}, 200);
+        	}
+        }
+
+        // Start the typing effect on load
+        _INTERVAL_VAL = setInterval(Type, 100);
+
+      });
     return (
         <div
           style={{
@@ -12,15 +71,13 @@ function HomePage() {
           }}
         >
           <Parallax pages={2}>
-          <ParallaxLayer offset={0} speed={0.2} style={{ opacity: 1, width:"auto", marginLeft: "35%", marginTop: "12vh" }}>
+          <ParallaxLayer offset={0} speed={0.2} style={{ opacity: 1, width:"auto", marginLeft: "40%", marginTop: "12vh" }}>
               <img
                 alt="self portrait"
                 src={require('../assets/sarah.png')}
-                style={{ width: "35%" }}
+                style={{ width: "40%" }}
               />
           </ParallaxLayer>
-
-
 
             <ParallaxLayer offset={0} speed={-0.1} style={{ opacity: 0.6, marginTop: "-8vh" }}>
               <img
@@ -41,11 +98,11 @@ function HomePage() {
             <ParallaxLayer offset={0} speed={-0.1} style={{ opacity: 1, width:"auto", marginLeft: "-40%", marginTop: "-8vh" }}>
             <div style={{ marginLeft: "10%", display: "inline-block"}}>
                 <h1
-                  style={{ marginTop: "35vh", fontSize: "82px", fontWeight: "800" }}
+                  style={{ marginTop: "35vh", fontSize: "92px", fontWeight: "900" }}
                 >SARAH XIE</h1>
-                  <p
-                    style={{ fontFamily: "space mono, sans-serif", color: "#00008b", textAlign: "right", marginLeft: "15px" }}
-                  >section that deletes and retypes</p>
+                  <div style={{ fontFamily: "space mono, sans-serif", color: "#00008b", textAlign: "right", marginLeft: "15px" }} id="container">
+                    	<div id="typeText"></div>
+                   </div>
               </div>
             </ParallaxLayer>
 
